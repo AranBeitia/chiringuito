b1ae2b69d8fe001b6ebeea0b54513a0505160bc7
AranBeitia <arantza.beitia@gmail.com>
2021-07-09 15:00:12 +0200

    validation

--- js/main.js
+++ js/main.js
@@ -9,3 +9,21 @@ function goToShopping() {
     shoppingChart.classList.toggle("is-visible");
     productChart.classList.toggle("is-hidden");
 }
+
+/**  VALIDATIONS */
+const nameInput = document.querySelector('input');
+// const form = document.querySelector('form');
+
+nameInput.addEventListener('input', () => {
+  nameInput.setCustomValidity('');
+  nameInput.checkValidity();
+});
+
+nameInput.addEventListener('invalid', () => {
+  if(nameInput.value === '') {
+    nameInput.setCustomValidity('este campo es requerido!, no te dejo pasar');
+  } else {
+    nameInput.setCustomValidity('Sólo nombres con letras upper y lowercases. Vuelve a intentarlo!');
+  }
+});
+
