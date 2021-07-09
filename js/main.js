997d37ad6a1cc34cf2e209d3db42520b9dc63c69
AranBeitia <arantza.beitia@gmail.com>
2021-07-09 16:59:57 +0200

    shipping form

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ var shoppingChart = document.querySelector('#carrito')
 
 var formProfile = document.querySelector('#form-profile')
 var formAddress= document.querySelector('#form-address')
+var formShipping = document.querySelector('#form-shipping')
 
 function goToShopping() {
   shoppingChart.classList.replace('is-hidden', 'is-visible')
@@ -15,9 +16,9 @@ function goToShopping() {
 
 /**  VALIDATIONS */
 const nameInput = document.querySelector('#name')
-document.getElementById('button-next').addEventListener('click', sendForm)
+document.getElementById('button-next').addEventListener('click', goToAddress)
 
-function sendForm(e) {
+function goToAddress(e) {
   e.preventDefault();
   var valuename = nameInput.value
 
@@ -27,5 +28,21 @@ function sendForm(e) {
   } else {
     formProfile.classList.add('is-hidden')
     formAddress.classList.replace('is-hidden', 'is-visible')
+  }
+}
+
+const addressInput = document.querySelector('#address')
+document.getElementById('button-next-shipping').addEventListener('click', goToShipping)
+
+function goToShipping(e) {
+  e.preventDefault();
+  var value = addressInput.value
+
+  let regex = /[A-Za-z]+/
+  if(value=== '' || !regex.test(value) ) {
+    alert('no vale')
+  } else {
+    formAddress.classList.replace('is-visible', 'is-hidden')
+    formShipping.classList.add('is-visible')
   }
 }
