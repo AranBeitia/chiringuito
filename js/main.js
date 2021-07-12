8eb2040a0796b5ec271c8b04ab6a3e0393cb9b9f
AranBeitia <arantza.beitia@gmail.com>
2021-07-12 21:04:54 +0200

    Structure code

--- js/main.js
+++ js/main.js
@@ -1,48 +1,62 @@
-var btnBuy = document
-	.querySelector('#to-buy')
-	.addEventListener('click', goToShopping)
+import {
+	productChart,
+	shoppingChart,
+	formProfile,
+	formAddress,
+	formShipping,
+	formEnd,
+	nameInput,
+	addressInput,
+	shippingInput,
+} from './variables.js'
 
-var productChart = document.querySelector('#producto')
-var shoppingChart = document.querySelector('#carrito')
-
-var formProfile = document.querySelector('#form-profile')
-var formAddress= document.querySelector('#form-address')
-var formShipping = document.querySelector('#form-shipping')
+document.querySelector('#to-buy').addEventListener('click', goToShopping)
+document.getElementById('button-next').addEventListener('click', goToAddress)
+document
+	.getElementById('button-next-shipping')
+	.addEventListener('click', goToShipping)
+document.getElementById('button-next-end').addEventListener('click', goToEnd)
 
 function goToShopping() {
-  shoppingChart.classList.replace('is-hidden', 'is-visible')
-  productChart.classList.add('is-hidden')
+	shoppingChart.classList.replace('is-hidden', 'is-visible')
+	productChart.classList.add('is-hidden')
 }
-
-/**  VALIDATIONS */
-const nameInput = document.querySelector('#name')
-document.getElementById('button-next').addEventListener('click', goToAddress)
 
 function goToAddress(e) {
-  e.preventDefault();
-  var valuename = nameInput.value
+	e.preventDefault()
+	var valuename = nameInput.value
 
-  let regex = /[A-Za-z]+/
-  if(valuename === '' || !regex.test(valuename) ) {
-    alert('no vale')
-  } else {
-    formProfile.classList.add('is-hidden')
-    formAddress.classList.replace('is-hidden', 'is-visible')
-  }
+	let regex = /[A-Za-z]+/
+	if (valuename === '' || !regex.test(valuename)) {
+		alert('no vale')
+	} else {
+		formProfile.classList.add('is-hidden')
+		formAddress.classList.replace('is-hidden', 'is-visible')
+	}
 }
 
-const addressInput = document.querySelector('#address')
-document.getElementById('button-next-shipping').addEventListener('click', goToShipping)
-
 function goToShipping(e) {
-  e.preventDefault();
-  var value = addressInput.value
+	e.preventDefault()
+	var value = addressInput.value
 
-  let regex = /[A-Za-z]+/
-  if(value=== '' || !regex.test(value) ) {
-    alert('no vale')
-  } else {
-    formAddress.classList.replace('is-visible', 'is-hidden')
-    formShipping.classList.add('is-visible')
-  }
+	let regex = /[A-Za-z]+/
+	if (value === '' || !regex.test(value)) {
+		alert('no vale')
+	} else {
+		formAddress.classList.replace('is-visible', 'is-hidden')
+		formShipping.classList.add('is-visible')
+	}
+}
+
+function goToEnd(e) {
+	e.preventDefault()
+	var value = shippingInput.value
+
+	let regex = /[A-Za-z]+/
+	if (value === '' || !regex.test(value)) {
+		alert('no vale')
+	} else {
+		formShipping.classList.replace('is-visible', 'is-hidden')
+		formEnd.classList.add('is-visible')
+	}
 }
---
+++ js/variables.js
@@ -0,0 +1,23 @@
+let productChart = document.querySelector('#producto')
+let shoppingChart = document.querySelector('#carrito')
+
+let formProfile = document.querySelector('#form-profile')
+let formAddress= document.querySelector('#form-address')
+let formShipping = document.querySelector('#form-shipping')
+let formEnd = document.querySelector('#form-end')
+
+const nameInput = document.querySelector('#name')
+const addressInput = document.querySelector('#address')
+const shippingInput = document.getElementById('shipping')
+
+export {
+  productChart,
+  shoppingChart,
+  formProfile,
+  formAddress,
+  formShipping,
+  formEnd,
+  nameInput,
+  addressInput,
+  shippingInput
+}
